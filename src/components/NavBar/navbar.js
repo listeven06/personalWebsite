ba89264e5b39aa0d4bbbf82ea214feee84ffb395
listeven06 <listeven06@gmail.com>
2026-01-01 18:15:05 -0800

    Initial Commit

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './navbar.css';
+import invertedLogo from '../../assets/invertedLogo.png'
+import { Link } from 'react-scroll';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <img src={invertedLogo} alt='logo' className='logo'/>
+            <div className='desktopMenu'>
+                <Link className='desktopMenuListItem'>Home</Link>
+                <Link className='desktopMenuListItem'>About</Link>
+                <Link className='desktopMenuListItem'>Projects</Link>
+                <Link className='desktopMenuListItem'>Resume</Link>
+                <Link className='desktopMenuListItem'>Cover Letter</Link>
+                <Link className='desktopMenuListItem'>Contact</Link>
+            </div>
+            <button className='desktopMenuBtn'>
+                <img src='' alt='' className='desktopMenuImg' />Contact Me
+            </button>
+        </nav>
+    )
+}
+
+export default Navbar
